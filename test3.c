510302aa6207b1e23be9ead9a8431614e622911a
EnzeXu <xue20@wfu.edu>
2021-10-18 16:11:28 -0400

    changes

--- test3.c
+++ test3.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <memory.h>
 
 void printArgv(char *argv[]) { // test print function
 	int i = 0;
