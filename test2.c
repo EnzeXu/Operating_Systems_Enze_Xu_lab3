6bc06bb52ef4448ab12015ca664792210412decd
EnzeXu <xue20@wfu.edu>
2021-10-18 18:56:47 -0400

    changes

--- test2.c
+++ test2.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
-#include <readline/history.h>
 
 int main(void)
 {
-    while (1) {
-        char* str = readline("shell: ");
-		if (strlen(str) > 1) {
-			 add_history(str);
-		}
-        free(str);
-    }
+	while (1)
+	{
+		char * str = readline("Myshell $ ");
+		add_history(str);
+		free(str);
+	}
 	return 0;
 }
