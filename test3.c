15ee3a1881a917141170104978f813d8a02f8470
EnzeXu <xue20@wfu.edu>
2021-10-18 15:26:59 -0400

    update

---
+++ test3.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int callCommandWithPipe(char *argv[], int left, int right) {
+	if (left >= right) return -1;
+	/* 判断是否有管道命令 */
+	int pipeIdx = -1;
+	for (int i=left; i<right; ++i) {
+		if (strcmp(commands[i], COMMAND_PIPE) == 0) {
+			pipeIdx = i;
+			break;
+		}
+	}
+	if (pipeIdx == -1) { // 不含有管道命令
+		return callCommandWithRedi(left, right);
+	} else if (pipeIdx+1 == right) { // 管道命令'|'后续没有指令，参数缺失
+		return ERROR_PIPE_MISS_PARAMETER;
+	}
+
+	/* 执行命令 */
+	int fds[2];
+	if (pipe(fds) == -1) {
+		return ERROR_PIPE;
+	}
+	int result = RESULT_NORMAL;
+	pid_t pid = vfork();
+	if (pid == -1) {
+		result = ERROR_FORK;
+	} else if (pid == 0) { // 子进程执行单个命令
+		close(fds[0]);
+		dup2(fds[1], STDOUT_FILENO); // 将标准输出重定向到fds[1]
+		close(fds[1]);
+
+		result = callCommandWithRedi(left, pipeIdx);
+		exit(result);
+	} else { // 父进程递归执行后续命令
+		int status;
+		waitpid(pid, &status, 0);
+		int exitCode = WEXITSTATUS(status);
+
+		if (exitCode != RESULT_NORMAL) { // 子进程的指令没有正常退出，打印错误信息
+			char info[4096] = {0};
+			char line[BUF_SZ];
+			close(fds[1]);
+			dup2(fds[0], STDIN_FILENO); // 将标准输入重定向到fds[0]
+			close(fds[0]);
+			while(fgets(line, BUF_SZ, stdin) != NULL) { // 读取子进程的错误信息
+				strcat(info, line);
+			}
+			printf("%s", info); // 打印错误信息
+
+			result = exitCode;
+		} else if (pipeIdx+1 < right){
+			close(fds[1]);
+			dup2(fds[0], STDIN_FILENO); // 将标准输入重定向到fds[0]
+			close(fds[0]);
+			result = callCommandWithPipe(pipeIdx+1, right); // 递归执行后续指令
+		}
+	}
+
+	return result;
+}
+
+
+int main() {
+	return 0;
+}
