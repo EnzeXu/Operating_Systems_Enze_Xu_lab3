0466cb9b289be8f23302e5e036baa8c68852a8b0
EnzeXu <xue20@wfu.edu>
2021-10-18 20:12:15 -0400

    changes

--- test6.c
+++ test6.c
@@ -3,60 +3,33 @@
 #include <arpa/inet.h>
 #include <string.h>
 
-#define MAX_LENGTH 254
+#define MAXN 100
 
-int main(void)
-{
-    char ipAddr[MAX_LENGTH];
+char *getIp(void) {
+	char ipAddr[MAXN] = {};
+	struct ifaddrs *ifAddrStruct = NULL;
+	void * tmpAddrPtr = NULL;
+	struct ifaddrs *iter = ifAddrStruct;
+	while (iter != NULL) {
+		if (iter->ifa_addr->sa_family == AF_INET) {
+			tmpAddrPtr = &((struct sockaddr_in *)iter->ifa_addr)->sin_addr;
+			char addressBuffer[INET_ADDRSTRLEN];
+			inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
+			if (strlen(ipAddr) + strlen(addressBuffer) < MAX_LENGTH - 1) {
+				strcpy(ipAddr, addressBuffer);
+			} else {
+				strcpy(ipAddr, "127.0.0.1");
+				break;
+			}
+		}
+		iter = iter->ifa_next;
+	}
+	freeifaddrs(ifAddrStruct);
+	return ipAddr;
+}
 
-    ipAddr[0] = '\0';
-
-    struct ifaddrs * ifAddrStruct = NULL;
-    void * tmpAddrPtr = NULL;
-
-    if (getifaddrs(&ifAddrStruct) != 0)
-    {
-        //if wrong, go out!
-        printf("Somting is Wrong!\n");
-        return -1;
-    }
-
-    struct ifaddrs * iter = ifAddrStruct;
-
-    while (iter != NULL) {
-        if (iter->ifa_addr->sa_family == AF_INET) { //if ip4
-            // is a valid IP4 Address
-            tmpAddrPtr = &((struct sockaddr_in *)iter->ifa_addr)->sin_addr;
-            char addressBuffer[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
-            if (strlen(ipAddr) + strlen(addressBuffer) < MAX_LENGTH - 1)
-            {
-                if (strlen(ipAddr) > 0)
-                {
-                     strcat(ipAddr, ";");
-                }
-                strcat(ipAddr, addressBuffer);
-            }
-            else
-            {
-                printf("Too many ips!\n");
-                break;
-            }
-        }
-        //else if (ifaddrstruct->ifa_addr->sa_family == af_inet6) { // check it is ip6
-
-        /* deal ip6 addr */
-        //    tmpaddrptr = &((struct sockaddr_in *)ifaddrstruct->ifa_addr)->sin_addr;
-        //    char addressbuffer[inet6_addrstrlen];
-        //    inet_ntop(af_inet6, tmpaddrptr, addressbuffer, inet6_addrstrlen);
-
-        //}
-        iter = iter->ifa_next;
-    }
-    //releas the struct
-    freeifaddrs(ifAddrStruct);
-
-    printf("The ips: %s\n", ipAddr);
-
-    return 0;
+int main() {
+	char *x = getIp();
+	printf("%s", x);
+	return 0;
 }
