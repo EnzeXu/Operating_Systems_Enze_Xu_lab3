471839a14036495b6493e00164471acecff00650
EnzeXu <xue20@wfu.edu>
2021-10-23 16:04:40 -0400

    test

--- test7.c
+++ test7.c
@@ -6,9 +6,11 @@ int main(void)
 		int c;
 		while (1) {
 			printf("inputs: ");
-			system("stty raw");
+			//system("stty raw");
+			system("stty -echo");
 			c = getchar();
-			system("stty -raw");
+			//system("stty -raw");
+			system("stty echo");
 			printf( "%c%c%c", c, c, c);
 			c = getchar();
 		}
