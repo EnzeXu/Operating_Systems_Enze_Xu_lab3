84f1201577f76bce4384cb5e447e3555379566e6
EnzeXu <xue20@wfu.edu>
2021-10-24 16:21:14 -0400

    test

--- test9.c
+++ test9.c
@@ -3,6 +3,7 @@
 int main() {
 	printf("hello\r\033[kworld!\n");
 	printf("hello\bworld!\n");
+	printf("hello\r\bworld!\n");
 	printf("hello\t\bworld!\n");
 	printf("hello%cworld!\n", 8);
 	return 0;
